7cbaf7c5e4bfc11a7d6caefc173ab620e80f4003
Mark <Mark@MARK-PC-NEW>
2018-11-19 15:30:14 +0200

    first changes

--- DDD.InPractice.Logic/SnackMachine.cs
+++ DDD.InPractice.Logic/SnackMachine.cs
@@ -8,6 +8,23 @@ namespace DDD.InPractice.Logic
 {
     public sealed class SnackMachine
     {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
 
+
+        public void InsertMoney(Money money)
+        {
+            MoneyInside += money;
+        }
+
+        public void ReturnMoney()
+        {
+            //should be implemented
+        }
+
+        public void BuySnack()
+        {
+            MoneyInTransaction += MoneyInside;
+        }
     }
 }
